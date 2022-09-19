72758c261e0e98dfebeddf49348b1df0e0a05f10
Hoang Trung <Trunghoang5322@gmail.com>
2022-09-19 15:49:20 +0700

    Add project music player

---
+++ assets/js/songs.js
@@ -0,0 +1,92 @@
+const songs = [
+    {
+        name : 'Dancing With Your Ghost',
+        singer: 'Sasha Sloan',
+        path: './assets/musics/song_1.mp3',
+        image: './assets/images/song_1.jpg'
+    },
+
+    {
+        name : 'Dusk Till Dawn',
+        singer: 'Zayn-Sia',
+        path: './assets/musics/song_2.mp3',
+        image: './assets/images/song_2.jfif'
+    },
+
+    {
+        name : 'Into Your Arms',
+        singer: 'Witt Lowry - Ava Max',
+        path: './assets/musics/song_3.flac',
+        image: './assets/images/song_3.jfif'
+    },
+
+    {
+        name : 'Let Me Down Slowly',
+        singer: 'Alec Benjamin',
+        path: './assets/musics/song_4.flac',
+        image: './assets/images/song_4.jfif'
+    },
+
+    {
+        name : 'Lost',
+        singer: 'Illenium - Emilie Brandt',
+        path: './assets/musics/song_5.m4a',
+        image: './assets/images/song_5.jpg'
+    },
+
+    {
+        name : 'Mine Illenium',
+        singer: 'Phoebe Ryan',
+        path: './assets/musics/song_6.mp3',
+        image: './assets/images/song_6.jfif'
+    },
+
+    {
+        name : 'On My Way',
+        singer: 'Alan Walker - Sabrina Carpenter - Farruko',
+        path: './assets/musics/song_7.mp3',
+        image: './assets/images/song_7.jfif'
+    },
+    
+    {
+        name : 'Rise',
+        singer: 'Jonas Blue - Jack Jack',
+        path: './assets/musics/song_8.flac',
+        image: './assets/images/song_8.jfif'
+    },
+
+    {
+        name : 'Somebody That I Used To Know',
+        singer: 'Gotye - Ki',
+        path: './assets/musics/song_9.flac',
+        image: './assets/images/song_9.jpg'
+    },
+
+    {
+        name : 'Takeaway',
+        singer: 'TheChainsmokers - Illenium - LennonStella',
+        path: './assets/musics/song_10.mp3',
+        image: './assets/images/song_10.jfif'
+    },
+
+    {
+        name : 'The Way I Still Love You',
+        singer: 'Reynard Silva',
+        path: './assets/musics/song_11.mp3',
+        image: './assets/images/song_11.jpg'
+    },
+
+    {
+        name : 'Unity',
+        singer: 'Alan Walker',
+        path: './assets/musics/song_12.flac',
+        image: './assets/images/song_12.jfif'
+    },
+
+    {
+        name : 'Unstoppable',
+        singer: 'Sia',
+        path: './assets/musics/song_13.flac',
+        image: './assets/images/song_13.jpg'
+    }
+];
